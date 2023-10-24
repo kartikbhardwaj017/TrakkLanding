95689a6a284f39f93e30be2734dbebb3c9effa12
kartikBhardwajPlum <kartik.b@plumhq.com>
2023-10-24 16:50:16 +0530

    init

---
+++ src/components/FAQ.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+const FAQData = [
+  {
+    question: "How does Trakk ensure my data is secure?",
+    answer:
+      "Your security and privacy are paramount to us. Trakk processes all data locally on your web app. This means your files or transaction details are not sent to any server, ensuring your data stays solely with you.",
+  },
+  {
+    question: "Can I manage multiple bank accounts with Trakk?",
+    answer:
+      "Absolutely! Trakk supports multi-account management.You can  switch between different bank accounts with ease by swiping down on Account card.",
+  },
+  {
+    question:
+      "Can I categorize my transactions based on type, like groceries or entertainment?",
+    answer:
+      "Yes, you can! Trakk offers a feature where you can attach categories to the recipient/payee of the transaction. Once you label a recipient, all similar transactions inherit that label automatically. You can select from predefined categories such as Entertainment, Groceries, Salary, and more from a dropdown menu.",
+  },
+  {
+    question:
+      "I'm not seeing my bank in the list of supported banks. Will more banks be added in the future?",
+    answer:
+      "As of now, Trakk supports major Indian banks. We are constantly working to expand our list of supported banks. If your bank isn't currently on our list, please reach out at kartikbhardwaj016@gmail.com, and we'll consider it for future updates.",
+  },
+];
+
+export const FAQ = () => (
+  <section className="relative pt-16 pb-16 bg-blueGray-50 overflow-hidden">
+    <div className="absolute -top-10" id="FAQ" />
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5, delay: 0.2 }}
+    >
+      <div className="relative z-10 container px-2 sm:px-8 lg:px-4 mx-auto w-11/12 sm:w-full">
+        <div className="md:max-w-4xl mx-auto">
+          <p className="mb-7 custom-block-subtitle text-center">
+            Have any questions?
+          </p>
+          <h2 className="mb-16 custom-block-big-title text-center">
+            Frequently Asked Questions
+          </h2>
+          <div className="mb-11 flex flex-wrap -m-1">
+            {FAQData.map((item, index) => (
+              <div className="w-full p-1">
+                <FAQBox
+                  title={item.question}
+                  content={item.answer}
+                  key={`${item.question}-${item.answer}`}
+                  defaultOpen={index === 0}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  </section>
+);
+
+const FAQBox = ({ defaultOpen, title, content }) => {
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+
+  return (
+    <div
+      className="pt-2 sm:pt-6 pb-2 px-3 sm:px-8  rounded-3xl bg-customDarkBg3 custom-border-gray-darker mb-4 relative hover:bg-customDarkBg3Hover cursor-pointer"
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <div className="flex flex-col p-2  justify-center items-start">
+        <h3 className=" custom-content-title pt-3 sm:pt-0 pr-8 sm:pr-0">
+          {title}
+        </h3>
+        <p
+          className={`text-customGrayText pt-4 transition-all duration-300 overflow-hidden ${
+            isOpen ? "max-h-96" : "max-h-0"
+          }`}
+        >
+          {content}
+        </p>
+      </div>
+      <div className="absolute top-6 right-4 sm:top-8 sm:right-8">
+        <svg
+          width="28px"
+          height="30px"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={`transition-all duration-500  ${
+            isOpen ? "rotate-[180deg]" : "rotate-[270deg]"
+          }`}
+        >
+          <path
+            d="M4.16732 12.5L10.0007 6.66667L15.834 12.5"
+            stroke="#4F46E5"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,78 @@
+import { motion } from "framer-motion";
+
+import { QuoteIcon } from "../assets/icons/QuoteIcon";
+import testimonial1 from "../assets/images/testimonial1.png";
+import testimonial2 from "../assets/images/testimonial2.png";
+import testimonial3 from "../assets/images/testimonial3.png";
+
+const testimonialsData = [
+  {
+    customerName: "Vinay Sharma",
+    customerTitle: "Founder of Dashflow",
+    content:
+      "Trakk has changed the way I look at my finances. It's not just an app; it's a financial companion.",
+    image: testimonial1,
+  },
+  {
+    customerName: "Ritik Bhardwaj",
+    customerTitle: "Founder of Dashflow",
+    content:
+      "I've tried multiple expense trackers, but Trakk stands out. The bar graphs are a game-changer!",
+    image: testimonial2,
+  },
+  {
+    customerName: "Randhir Sharma",
+    customerTitle: "Founder of Dashflow",
+    content:
+      "Before Trakk, I was always wondering where my money went. Now, I have a clear picture and can make informed decisions. A lifesaver!",
+    image: testimonial3,
+  },
+];
+
+export const Testimonials = () => (
+  <section className="w-full flex justify-center pt-10 mb-16 lg:mb-32 bg-customDarkBg2 relative">
+    <div className="absolute -top-16" id="feedback" />
+    <div className="flex flex-col w-full lg:w-[1150px] justify-center">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.4, delay: 0.3 }}
+      >
+        <div className="custom-block-subtitle text-center mb-6">
+          Testimonials
+        </div>
+        <div className="custom-block-big-title text-center mb-16 px-8 sm:px-24 md:px-48">
+          People like you love Trakk
+        </div>
+
+        <div className="flex flex-col lg:flex-row gap-8 lg:gap-5 xl:gap-10 px-6 xl:px-0 items-center">
+          {testimonialsData.map((testimonial, index) => (
+            <div
+              className="w-11/12 sm:w-4/5 md:w-[560px] lg:w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4"
+              key={`${testimonial.customerName}-${index}`}
+            >
+              <div className="flex mb-2">
+                <QuoteIcon />
+              </div>
+              <div className="custom-content-text-white">
+                "{testimonial.content}"
+              </div>
+              <div className="flex mt-4 mb-2 xl:mt-8 xl:mb-4">
+                <img src={testimonial.image} alt="" width="45px" />
+                <div className="flex flex-col ml-4">
+                  <div className="custom-content-text-white font-medium">
+                    {testimonial.customerName}
+                  </div>
+                  <div className="custom-content-text-gray">
+                    {testimonial.customerTitle}
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </motion.div>
+    </div>
+  </section>
+);
