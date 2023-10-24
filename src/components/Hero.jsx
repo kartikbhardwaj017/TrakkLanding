669432765f63dd872052a1fda05c9256585c5fc1
kartikBhardwajPlum <kartik.b@plumhq.com>
2023-10-24 17:54:15 +0530

    update hero text and redirect to trakk app

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.05 }}
         >
           <div className="text-5xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-8 md:px-20 lg:px-4">
-            <span className="inline md:hidden">Analytics</span>
+            <span className="inline md:hidden">Budgeting</span>
             <span className="hidden md:inline">Expense Management,</span>
           </div>
           <div className="mt-2 sm:mt-2 text-4xl sm:text-6xl lg:text-7xl xl:text-7xl font-bold tracking-wide  text-white  px-8 sm:px-20 md:px-24 lg:px-24">
@@ -41,8 +41,8 @@ export const Hero = () => {
           transition={{ duration: 0.5, delay: 0.1 }}
         >
           <div className="text-customGrayText text-sm lg:text-base xl:text-lg sm:text-base mt-10 px-12 sm:px-48 ">
-            Meticulously designed to give you a seamless and visually appealing tracking
-            experience.
+            Meticulously designed to give you a seamless and visually appealing
+            expense tracking experience.
           </div>
         </motion.div>
         <motion.div
@@ -53,15 +53,11 @@ export const Hero = () => {
           <div className="flex flex-col gap-2 sm:flex-row mt-14 mb-24 sm:mb-40 justify-center">
             <div
               className="custom-button-colored w-64 sm:w-52 h-12 mr-0 sm:mr-4 lg:mr-6 mb-2 sm:mb-0"
-              onClick={() => setIsModalOpen(true)}
+              onClick={() =>
+                (window.location.href = "https://app.trakk.money/upload")
+              }
             >
               Get Started
-            </div>
-            <div
-              className="w-64 sm:w-52 h-12 rounded-xl font-bold text-white border border-solid  flex justify-center items-center cursor-pointer bg-customDarkBg2 hover:bg-customDarkBg3 border-customPrimary transition"
-              onClick={() => setIsModalOpen(true)}
-            >
-              Live demo
             </div>
           </div>
         </motion.div>
